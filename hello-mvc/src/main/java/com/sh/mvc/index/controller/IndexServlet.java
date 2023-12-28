65ea321528455cd2930912d11015b5c2342f590f
jjaejun <leejj3240@gmail.com>
2023-12-28 17:57:51 +0900

    Feat: oracle cloud db 연동

--- hello-mvc/src/main/java/com/sh/mvc/index/controller/IndexServlet.java
+++ hello-mvc/src/main/java/com/sh/mvc/index/controller/IndexServlet.java
@@ -1,7 +1,5 @@
 package com.sh.mvc.index.controller;
 
-import com.sh.mvc.member.model.entity.Member;
-import com.sh.mvc.notification.model.entity.Notification;
 import com.sh.mvc.notification.model.service.NotificationService;
 
 import javax.servlet.ServletException;
@@ -10,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("")
 public class IndexServlet extends HttpServlet {
@@ -20,11 +17,11 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 로그인 후 필요한 데이터를 로드해서 jsp 전달
-        Member loginMember = (Member) req.getSession().getAttribute("loginMember");
-        if (loginMember != null) {
-            List<Notification> notifications = notificationService.findByMemberId(loginMember.getId());
-            req.setAttribute("notifications", notifications);
-        }
+//        Member loginMember = (Member) req.getSession().getAttribute("loginMember");
+//        if (loginMember != null) {
+//            List<Notification> notifications = notificationService.findByMemberId(loginMember.getId());
+//            req.setAttribute("notifications", notifications);
+//        }
 
         req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
